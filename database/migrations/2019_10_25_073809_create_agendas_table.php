a590176f8a26a9514cff549e7b508106cb4e9832
eliabrian <eliabrian69@gmail.com>
2019-10-25 14:41:10 +0700

    Agenda Model and Migration + NPM

---
+++ database/migrations/2019_10_25_073809_create_agendas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('desc');
+            $table->text('image');
+            $table->integer('date');
+            $table->string('publisher');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
